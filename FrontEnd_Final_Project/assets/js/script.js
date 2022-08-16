161aaf9d80aad7f3356216c06cc331df4879d17f
Rashadfrd <105360932+Rashadfrd@users.noreply.github.com>
2022-08-16 22:12:22 +0400

    Add files via upload

--- FrontEnd_Final_Project/assets/js/script.js
+++ FrontEnd_Final_Project/assets/js/script.js
@@ -1,10 +1,57 @@
 window.onscroll = function(){myFunction()};
-let content = document.querySelector(".content")
+let leftBtn = document.querySelector(".leftbtn");
+let rightBtn = document.querySelector(".rightbtn");
+let delivery = document.querySelector(".delivery");
+let buying = document.querySelector(".buying");
+let personalSession = document.querySelector(".personal-session");
+let giftVoucher = document.querySelector(".gift-voucher");
+let deliveryImg = document.querySelector(".delivery-img")
+let buyingImg = document.querySelector(".buying-img")
+let personalSessionImg = document.querySelector(".personal-img")
+let giftVoucherImg = document.querySelector(".gift-voucher-img")
+
+
+delivery.addEventListener("mouseenter",function (e) {
+  e.stopPropagation();
+  deliveryImg.style.animationName = "mouseEnter";
+})
+delivery.addEventListener("mouseleave",function (e) {
+  e.stopPropagation();
+  deliveryImg.style.animationName = "mouseLeave";
+})
+
+buying.addEventListener("mouseenter",function (e) {
+  e.stopPropagation();
+  buyingImg.style.animationName = "mouseEnter";
+})
+buying.addEventListener("mouseleave",function (e) {
+  e.stopPropagation();
+  buyingImg.style.animationName = "mouseLeave";
+})
+
+personalSession.addEventListener("mouseenter",function (e) {
+  e.stopPropagation();
+  personalSessionImg.style.animationName = "mouseEnter";
+})
+personalSession.addEventListener("mouseleave",function (e) {
+  e.stopPropagation();
+  personalSessionImg.style.animationName = "mouseLeave";
+})
+
+giftVoucher.addEventListener("mouseenter",function (e) {
+  e.stopPropagation();
+  giftVoucherImg.style.animationName = "mouseEnter";
+})
+giftVoucher.addEventListener("mouseleave",function (e) {
+  e.stopPropagation();
+  giftVoucherImg.style.animationName = "mouseLeave";
+})
+
 
 function myFunction() {
-    if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
+    if (document.body.scrollTop > 53 || document.documentElement.scrollTop > 53) {
         // $("#header").show(500);
-        $("#header").slideDown()
+        $("#header").show();
         // $("#header-2").hide();
       } 
       else{
