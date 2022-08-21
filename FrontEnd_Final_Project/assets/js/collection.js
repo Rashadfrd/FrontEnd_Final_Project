37aff1248cd6bdb8b42d63928e87a517fc89ebd9
Rashadfrd <105360932+Rashadfrd@users.noreply.github.com>
2022-08-22 03:33:28 +0400

    Add files via upload

--- FrontEnd_Final_Project/assets/js/collection.js
+++ FrontEnd_Final_Project/assets/js/collection.js
@@ -5,3 +5,25 @@ $(".img-discount").mouseenter(function () {
 $(".img-discount").mouseleave(function () {
     $(".box").hide(300)
 })
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    }
+})
